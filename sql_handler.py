5d92d129f7f86c36c03290b48778c1b1bd0c462c
Ella <Ella.shan@computingstack.com>
2025-02-19 12:22:23 -0800

    SQL data base handler

---
+++ sql_handler.py
@@ -0,0 +1,28 @@
+from urllib.parse import quote_plus
+import datetime
+import pandas as pd
+from sqlalchemy import create_engine, event
+
+
+# CONNECT=mssql+pyodbc:///?odbc_connect={}".format(
+#     quote_plus(
+#         "DRIVER={SQL Server};SERVER=.\SQLSERVER2019;DATABASE=Countries;"
+#         "Trusted_Connection=yes;"
+#     )
+# )
+engine = create_engine(CONNECT)
+
+file="data.csv"
+@event.listens_for(engine, "before_cursor_execute")
+def receive_before_cursor_execute(
+    conn, cursor, statement, params, context, executemany
+):
+    if executemany:
+        cursor.fast_executemany = True
+
+
+csv_file = pd.read_csv(file)
+
+csv_file.to_sql(file,engine, if_exsit="Append",index=False,chunksize=1000)
+
+print("data exported to SQL server!")
