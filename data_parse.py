dbccd7b6390bf738d15abfbfa1e7439f86ad2e8c
Ella <Ella.shan@computingstack.com>
2025-03-21 15:43:19 -0700

    Add files via upload

--- data_parse.py
+++ data_parse.py
@@ -1,18 +1,20 @@
 #READ ME!!
 # PLEASE PLACE ALL NORMAL FILES INTO A FOLDER CALLED 'data' UNDER "DOCUMENTS", WHICH IS WHERE THE PYTHON
 # AND VSCODE IS INSTALLED --> TO MAKE SURE PLEASE CHECK THE COMMAND LINE, YOU SHOULD SEE:
-# PS C:\Users\LG8223\OneDrive - EQUANS\Documents>
+# PS C:\Users\LG8223\OneDrive - EQUANS\Documents> or wherever you place your python file
+# Note this is for compiling the python file. The excel file does not need to be in the same folder or pathway, 
+# If not, please precise the full address of the excel file under the variable file_path
 # PLACE Wall Rouh-In.csv IN "DOCUMENTS"
 # THIS SERVES AS A DATA MANIPULATION ALGORITHM TO PARSE FOR PROGRESS TRACKING
 # FOR QUESTIONS, PLEASE CONTACT ella.lewis@equans.com or elcslewis@gmail.com
-# NOTE THE PROCESSING TIME MAY VARY DEPENDING ON THE SIZE OF RAW DATA.
+#  THE PROCESSING TIME MAY VARY DEPENDING ON THE SIZE OF  DATA.
 
 import os
 import datetime
 import pandas as pd
 import shutil
-file_path = "Wall Rough-In.csv"
-folder= 'data'
+import re
+file_path = "llc.csv"
 
 #function definition
 ###############1################## Normal data manipulation
@@ -72,7 +74,71 @@ def different(df, file_path):
     print("Uploading changes to file...")
     df.to_csv(file_path, index=False)
 
+def high(df, file_path):
+    print(file_path, "opened")
+    df=df.drop(columns=['ID', 'Parent',], axis=1, errors= 'ignore')
+    print("columsn dropped")
+    for index, text in df['Space'].items():
+        if isinstance(text,str):
+            place= text.rfind(')')
+            df.at[index, 'WBS']= text[place+1:].strip()
+            place2= text.rfind('(')
+            df.at[index, 'Pour']= text[:place2-1]
+    print("new columns created")
+    df['Pour'] = df['Pour'].str.replace("(Pour)", "", regex=False)
+    print("filter1")
+    df['WBS'] = df['WBS'].str.replace("(WBS)", "", regex=False)
+    df=df.drop(['Space'], axis=1, errors='ignore')
+    df['Pour']= df['Pour'].str.strip()
+    print("filter2")
+    print("Uploading changes to file...")
+    df.to_csv(file_path, index=False)
 
+
+def llc(df, file_path):
+    df=df.drop(columns=['ID', 'Parent'], axis=1, errors= 'ignore')
+    print("columsn dropped")
+    df['Status'] = df['Status'].str.replace("|Room with Constraints", "", regex=False)
+    df['Status'] = df['Status'].str.replace("|Internal", "", regex=False)
+    df['Status'] = df['Status'].str.replace("|Ready", "", regex=False)
+    for index, text in df['Space'].items():
+        if isinstance(text,str):
+            place= text.rfind(')')
+            df.at[index, 'Space']= text[:place-5]
+            df.at[index, 'AHU']= text[place+1:].strip()
+    df['WBS']= df['Space'].str[5:]
+
+
+    for index, text in df['Status'].items():
+        if isinstance(text,str):
+            place= text.rfind('|')
+            if place!=-1:
+                df.at[index, 'Status']= text[place:]
+
+    for index, text in df['Status'].items():
+        if isinstance(text,str):
+            place1=text.find('%')
+            if place!=1:
+                df.at[index, 'Ready']= text[:place1+1]
+    df['Ready'] = df['Ready'].str.replace("|", "", regex=False)
+    for index, text in df['Status'].items():
+        if isinstance(text,str):
+            if '%' not in text:
+                df.at[index,'Ready']=0
+    for index, text in df['Status'].items():
+        if isinstance(text, str) and any(word in text for word in ["Pass", "Ready"]):
+            df.at[index, 'Ready'] = '100%'
+    df['Ready'] = df['Ready'].str.replace("%", "", regex=False)
+    
+    for index, text in df['Ready'].items():
+        if isinstance(text,str):
+            df.at[index, 'Ready']=float(text)/100
+
+    df['Ready'] = df['Ready'].fillna(0)
+    df=df.drop("Status", axis=1, errors="ignore")
+    df = df[df['Subject'].isin(["Rectangle", "Polygon"])]
+    print("done")
+    df.to_csv(file_path, index=False)
 #RENAME FILE TO TODAY'S DATE
 def rename(file_path):
     dest = datetime.datetime.now()
@@ -83,16 +149,10 @@ def rename(file_path):
     shutil.move(new_filename, destination)
     print("File moved...")
 
-#Upload data for the rest of the files
-for filename in os.listdir(folder):
-    df1=pd.read_csv(filename)
-    normal(df1, filename)
-    print(filename+" successfully scraped...")
-    rename(filename)
-print("Uploaded into the data folder...")
 
 #Wall Rough-In upload --> access under Documents-- IMPORTANT: DO NOT PLACE IT INTO ANY FOLDER.
 df=pd.read_csv(file_path)
-different(df,file_path)
+
+llc(df,file_path)
 rename(file_path)
 print("uploaded! Program exiting...")
